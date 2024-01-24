e582396a05f512babec33d6de690f3a503c89787
Hakizimana Fred <hakifred20@gmail.com>
2024-01-24 21:59:07 +0300

    chore: button & button props

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,21 +1,29 @@
 import * as React from 'react';
 import { Text } from 'react-native';
-import { Heading, Wrapper } from 'reactnatively';
+import { Button, Heading, Wrapper } from 'reactnatively';
 
 export default function App() {
   return (
-    <Wrapper>
-      <Text>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quod nostrum
-        deserunt fuga voluptates quisquam, maxime voluptatibus tenetur
-        doloremque iusto porro laborum harum laboriosam quis perspiciatis cumque
-        incidunt quam itaque molestiae vero repellendus quia placeat libero
-        voluptatum! Ullam vitae laborum cumque eius harum debitis. Maiores ad
-        maxime ab aliquam deserunt explicabo.
-      </Text>
-      <Heading variant="h1" align="left">
-        Well done
-      </Heading>
-    </Wrapper>
+    <>
+      <Wrapper>
+        <Text>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quod nostrum
+          deserunt fuga voluptates quisquam, maxime voluptatibus tenetur
+          doloremque iusto porro laborum harum laboriosam quis perspiciatis
+          cumque incidunt quam itaque molestiae vero repellendus quia placeat
+          libero voluptatum! Ullam vitae laborum cumque eius harum debitis.
+          Maiores ad maxime ab aliquam deserunt explicabo.
+        </Text>
+        <Heading variant="h1" align="left">
+          Well done
+        </Heading>
+        <Button size="lg" onPress={() => console.log('sup with you')}>
+          Hi
+        </Button>
+      </Wrapper>
+      {/* <View style={{ width: 100 }}>
+        <Button size="lg">Hi</Button>
+      </View> */}
+    </>
   );
 }
